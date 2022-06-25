a0c782385591fa00a56c24599c5b3dd8f5e40a61
Samson Ahadji <samahadji@gmail.com>
2022-06-25 11:11:35 +0000

    Update routes

--- index.js
+++ index.js
@@ -1,13 +1,131 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose');
+const moment = require('moment')
+let bodyParser = require('body-parser');
+const { response } = require('express');
 require('dotenv').config()
 
+mongoose.connect(process.env.MONGO_URI);
+
+let userSchema = new mongoose.Schema({
+  username: String,
+  log: [{
+    description: String,
+    duration: Number,
+    date: Date
+  }]
+})
+
+/*let exerciseSchema = new mongoose.Schema({
+  description: String,
+  duration: Number,
+  date: Date
+})*/
+let User = mongoose.model('User', userSchema)
+//let Exercise = mongoose.model('Exercise', userSchema)
+
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors())
 app.use(express.static('public'))
+
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+
+app.get('/api/users', (req, res) => {
+  queryUsers({}, ['username', '_id'], (err, docs) => {
+    if (err) {res.json({error: err})}
+    else {
+      res.send(docs);
+    }
+  })
+});
+
+
+app.post('/api/users', (req, res) => {
+  newUserName = req.body.username;
+
+  createAndSaveUser({username: newUserName}, (err, data) => {
+    if (err) {res.json({error: err})} 
+    else { res.json({
+      username: data.username,
+      _id: data._id
+    })}
+  })
+  
+});
+
+app.post('/api/users/:_id/exercises', (req, res) => {
+  //add exercise
+  if (!req.params._id) return res.json({error: 'No user ID was specified'})
+  
+  User.findById(req.params._id, (err, user) => {
+    if (err) {res.json({error: err}) }
+    else {
+      let exerciseDate
+      if (req.body.date!=='') {exerciseDate = moment(req.body.date).format('ddd MMM DD YYYY')} else {exerciseDate = moment().format('ddd MMM DD YYYY')}
+      
+      user.log.push({
+        duration: req.body.duration,
+        description: req.body.description,
+        date: exerciseDate
+      });
+      user.save()
+        .then(doc => {
+          addedExercise = doc.log[doc.log.length-1]
+          res.json({
+            _id: doc._id,
+            username: doc.username,
+            description: addedExercise.description,
+            duration: addedExercise.duration,
+            date: moment(addedExercise.date).format('ddd MMM DD YYYY'),
+          })}
+        )
+        .catch(err => res.json({error: err})
+          )
+      }
+    })
+  })
+
+app.get('/api/users/:_id/logs', (req, res) => {
+  User.findById(req.params._id, (err, user) => {
+    if (err) {res.json({error: err}) }
+    else {
+      let logCount = user.log.length;
+      res.json({
+        username: user.username,
+        count: logCount,
+        _id: user._id,
+        log: [...user.log]
+      }) 
+    }
+  })
+});
+
+const createAndSaveUser = (user, done) => {
+    newUser = new User({
+    username: user.username,
+  }) 
+  newUser.save((err, data) => {
+    if (err) {
+      return console.error(err);
+    } else { 
+      done(null, data);
+  }
+   } ) 
+};
+
+const queryUsers = (options, select,  done) => {
+  User.find(options)
+  .sort({username: 'ascending'}) // or 'asc'
+  .select(select)
+  .exec((err, res) => {
+    if (err) { done(err, null); } else { done(null, res);}
+  })
+};
 
 
 
